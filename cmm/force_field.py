Multipole scaling. Fixing a bug with the allocation size of default-zero parameters.

cmm/force_field.py
@@ -1,5 +1,6 @@
 import torch, math
-from .multipole import computeCartesianQuadrupoles
+from .multipole import computeCartesianQuadrupoles, rotateMultipoles, rotateQuadrupoles
+from .short_range import scaleMultipoles
 from .coordinate_manager import CoordinateManager
 from .parameters import Parameterizer
 from .topology import Topology
@@ -111,8 +112,6 @@ class CMM(ForceField):
                 "ct_slope_1": torch.tensor([65.0]),
                 "ct_slope_2": torch.tensor([13.7812]),
                 "eps":torch.tensor([1.0 / 0.380979]),
-                "j_cf_bb": torch.tensor([-0.0332338]),
-                "k_hardness_bb": torch.tensor([0.958157]),
             },
         }
 
@@ -160,25 +159,61 @@ class CMM(ForceField):
         pairs, dists, dist_vecs = cm.get_distances_vectors_and_pairs()
         angles = computeAngleFromVecs(dist_vecs[topology.angle_pairs[0]], dist_vecs[topology.angle_pairs[1]])
 
+        # Electric Multipoles #
         q_shell = params.get_atomic_parameters('q_shell')
+        dipo = params.get_atomic_parameters('dipo')
+        quad = params.get_atomic_parameters('quad')
+        # Polarizability #
+        alpha = params.get_atomic_parameters('alpha')
+
+        # Pauli Multipoles #
         q_pauli = params.get_atomic_parameters('q_pauli')
+        Kdipo_pauli = params.get_atomic_parameters('Kdipo_pauli')
+        Kquad_pauli = params.get_atomic_parameters('Kquad_pauli')
+
+        # Exchange Polarization #
+        q_xpol = params.get_atomic_parameters('q_xpol')
+        Kdipo_xpol = params.get_atomic_parameters('Kdipo_xpol')
+        Kquad_xpol = params.get_atomic_parameters('Kquad_xpol')
+
+        # Charge Transfer Multipoles #
+        q_ct_acc = params.get_atomic_parameters('q_ct_acc')
+        Kdipo_ct_acc = params.get_atomic_parameters('Kdipo_ct_acc')
+        Kquad_ct_acc = params.get_atomic_parameters('Kquad_ct_acc')
+        q_ct_don = params.get_atomic_parameters('q_ct_don')
+        Kdipo_ct_don = params.get_atomic_parameters('Kdipo_ct_don')
+        Kquad_ct_don = params.get_atomic_parameters('Kquad_ct_don')
+
+        b = params.get_atomic_parameters('b')
+        b_pauli = params.get_atomic_parameters('b_pauli')
+        b_disp = params.get_atomic_parameters('b_disp')
+        b_xpol = params.get_atomic_parameters('b_xpol')
+        b_ct = params.get_atomic_parameters('b_ct')
+        eta = params.get_atomic_parameters('eta')
+
+        # HERE: I think the problem is that below produces a 3 for [1,1], which is fine. The problem is that
+        # the storage space (of zeros) is not large enough so that when we try to pull from the array we index
+        # out of bounds.
+        print(params._symmetric_pairing_function(torch.tensor([[0, 0], [0, 1], [1, 0], [1, 1]])))
+        #eps = params.get_pair_parameters('eps', topology.all_intermolecular_pairs) # Need to get intermolecular pairs?
         r_eq = params.get_pair_parameters('r_eq', topology.bonded_pairs)
         j_cf = params.get_pair_parameters('j_cf', topology.bonded_pairs)
         j_cf_pauli = params.get_pair_parameters('j_cf_pauli', topology.bonded_pairs)
+        k_hardness_b = params.get_pair_parameters('k_hardness_b', topology.bonded_pairs)
         
-        # NOTE(JOE): In a more general implementation, the bond-bond coupling parameters will not be
-        # identical for each bond in an angle. That is, suppose we have an OCN angle. When the CO
-        # distance changes, the CN charge flux need not be the same as the CO charge flux when the
-        # CN distance changes. This is why we need a separate function call, get_pair_pair_parameters.
-        # I think the below is correct since the angle type is non-symmetric in this way. Hard to verify
-        # without additional molecules.
+        # NOTE(JOE): Need to test that we get the right bond-bond parameters for non-symmetric angles.
+        # Currently, we don't have parameters for a non-symmetric angle but they will come up with
+        # organic molecules.
         r_eq_bb_1 = params.get_pair_parameters('r_eq', topology.angle_pairs[0])
         r_eq_bb_2 = params.get_pair_parameters('r_eq', topology.angle_pairs[1])
         j_cf_bb_1 = params.get_pair_pair_parameters('j_cf_bb', topology.angle_pairs[0], topology.angle_pairs[1])
         j_cf_bb_2 = params.get_pair_pair_parameters('j_cf_bb', topology.angle_pairs[1], topology.angle_pairs[0])
+        k_hardness_bb_1 = params.get_pair_pair_parameters('k_hardness_bb', topology.angle_pairs[0], topology.angle_pairs[1])
+        k_hardness_bb_2 = params.get_pair_pair_parameters('k_hardness_bb', topology.angle_pairs[1], topology.angle_pairs[0])
 
         theta_eq = params.get_angle_parameters('theta_eq', pairs, topology.angle_pairs)
         j_cf_angle = params.get_angle_parameters('j_cf_angle', pairs, topology.angle_pairs)
+        k_hardness_angle = params.get_angle_parameters('k_hardness_angle', pairs, topology.angle_pairs)
 
         # Pauli charge flux #
         evaluate_bond_charge_flux(pairs, dists, topology.bonded_pairs, q_pauli, r_eq, j_cf_pauli)
@@ -190,4 +225,33 @@ class CMM(ForceField):
             q_shell, r_eq, theta_eq, j_cf, j_cf_angle,
             r_eq_bb_1, r_eq_bb_2, j_cf_bb_1, j_cf_bb_2
         )
-        #print(q_shell)
+        
+        # Hardness change #
+        evaluate_hardness_change(
+            pairs, dists, angles,
+            topology.bonded_pairs, topology.angle_pairs, topology.angle_atoms,
+            eta, r_eq, theta_eq, k_hardness_b, k_hardness_angle,
+            r_eq_bb_1, r_eq_bb_2, k_hardness_bb_1, k_hardness_bb_2
+        )
+
+        # Rotation Matrices #
+        rotation_matrices = cm.compute_rotation_matrices(params.get_atomic_parameters("axistypes"))
+        
+        multipoles = rotateMultipoles(
+            q_shell, dipo, quad, rotation_matrices
+        ) * torch.tensor([1, 1, 1, 1, 1/3, 2/3, 2/3, 1/3, 2/3, 1/3])
+        polarizabilities = rotateQuadrupoles(alpha, rotation_matrices)
+
+        # SPEED: Makes copies. Might be unavoidable but could maybe be done more efficiently.
+        multipoles_ct_acc = scaleMultipoles(multipoles, q_ct_acc, Kdipo_ct_acc, Kquad_ct_acc)
+        multipoles_ct_don = scaleMultipoles(multipoles, q_ct_don, Kdipo_ct_don, Kquad_ct_don)
+        multipoles_pauli = scaleMultipoles(multipoles, q_pauli, Kdipo_pauli, Kquad_pauli)
+
+        #ct_direct_pairwise, dq_pairwise = computePairwiseChargeTransfer(
+        #    dist_vecs,
+        #    multipoles_ct_acc[pairs[0]], multipoles_ct_acc[pairs[1]],
+        #    multipoles_ct_don[pairs[0]], multipoles_ct_don[pairs[1]],
+        #    b_ct[pairs[0]], b_ct[pairs[1]],
+        #    self.nb_params['eps'][pairs[0], pairs[1]]
+        #)
+

cmm/terms.py
@@ -1,5 +1,4 @@
 import torch
-from .coordinate_manager import CoordinateManager
 from .bonded import *
 
 # This file provides convenient wrappers of combinations of computational kernels
@@ -53,3 +52,33 @@ def evaluate_bond_and_angle_charge_flux(
     flux_charges.scatter_add_(0, angle_atoms[:, 1], charge_flux_angle_list_j)
     flux_charges.scatter_add_(0, angle_atoms[:, 2], charge_flux_angle_list_k)
     q.add_(flux_charges)
+
+def evaluate_hardness_change(
+        pairs: torch.Tensor, dists: torch.Tensor, angles: torch.Tensor,
+        bonded_pairs: torch.Tensor, angle_pairs: torch.Tensor, angle_atoms: torch.Tensor,
+        eta: torch.Tensor, r_eq: torch.Tensor, theta_eq: torch.Tensor,
+        k_hardness_b: torch.Tensor,  k_hardness_angle: torch.Tensor,
+        r_eq_bb_1: torch.Tensor, r_eq_bb_2: torch.Tensor,
+        k_hardness_bb_1: torch.Tensor, k_hardness_bb_2: torch.Tensor):
+    
+    hardness_product = torch.ones_like(eta)
+    hardness_change_b = computeHardnessChangeBond(dists[bonded_pairs], r_eq, k_hardness_b)
+    hardness_change_bb_1, hardness_change_bb_2 = computeHardnessChangeBondBond(
+        dists[angle_pairs[0]], dists[angle_pairs[1]],
+        r_eq_bb_1, r_eq_bb_2, k_hardness_bb_1, k_hardness_bb_2
+    )
+    hardness_change_angle = computeHardnessChangeAngle(angles, theta_eq, k_hardness_angle)
+        
+    # NOTE(JOE): We only accumulate some of the bond-bond terms since this basically assumes that all of
+    # the hardness change is on the second atom of the bond. i.e. just the H atoms in water.
+    # This is yet another reason not to like this piece of the model. There should be a better way.
+
+    # Cannot do in-place operations or else computational graphs breaks. Sad.
+    bonded_atoms = pairs[bonded_pairs].T
+    hardness_product = hardness_product.scatter_reduce(0, bonded_atoms[1], hardness_change_b, reduce="prod")
+    hardness_product = hardness_product.scatter_reduce(0, pairs[angle_pairs[0]].T[1], hardness_change_bb_1, reduce="prod")
+    hardness_product = hardness_product.scatter_reduce(0, pairs[angle_pairs[1]].T[1], hardness_change_bb_2, reduce="prod")
+
+    eta *= hardness_product
+    eta.scatter_add_(0, angle_atoms.T[0], hardness_change_angle)
+    eta.scatter_add_(0, angle_atoms.T[2], hardness_change_angle)

cmm/topology.py
@@ -68,19 +68,27 @@ class Topology:
         
         pairs = nl.get_pairs()
         self._find_all_intramolecular_pairs(pairs)
+        self._find_all_intermolecular_pairs(pairs.size(0))
+
+    def _find_all_intermolecular_pairs(self, n_pairs):
+        """Compute set difference between intramolecular pairs array and torch.arange over n_pairs."""
+        all_pairs = torch.arange(n_pairs)
+        mask = ~torch.isin(all_pairs, self.all_intramolecular_pairs)
+        self.all_intermolecular_pairs = all_pairs[mask]
 
     def _find_all_intramolecular_pairs(self, pairs: torch.Tensor):
         # Once we have dihedral indices, we should use that instead.
         # Basically, this lets us easily get the indices of all pairs
         # which can be formed from the atoms forming angles/dihedrals.
         # e.g. we need to also capture the H-H pair in a water which
-        # is not bonded but is also not needed in the intermolecular
+        # is not bonded but should be ignored in the intermolecular
         # calculations.
         # These indexing shenanigans come from: https://stackoverflow.com/questions/73187923/applying-torch-combinations-on-multidimensional-tensor-or-tuple-of-tensors-in-py
         self.angle_atoms = torch.unique(torch.hstack((pairs[self.angle_pairs[0]], pairs[self.angle_pairs[1]])), dim=1)[:, [1, 0, 2]]
         # ^^^ It is unclear to me if the above re-ordering will always work or just for water. These indices are used
         # when evaluating angle-dependent parameters. If that seems to be a problem, then this ordering is probably not
         # guaranteed to be right. The solution is probably some standard sorting of the atoms for how angles are evaluated.
+        # i.e. some canonical ordering of the bond graph.
         # -Joe 1/3/25
 
         c = torch.combinations(torch.arange(self.angle_atoms.size(1)), r=2)