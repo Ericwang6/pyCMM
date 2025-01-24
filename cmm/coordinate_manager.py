Added test for mutliple evaliations in a row. Should now be able to successfully update coordinates and box and get correct gradients.

cmm/coordinate_manager.py
@@ -26,12 +26,15 @@ class CoordinateManager:
         # TODO: Allow for multiple cutoffs since short_range only needs roughly
         # 6 angstroms but vdw needs 12 angstroms usually. Also allow for choice
         # of neighbor list. We just use a cell list for now.
+        self._need_coordindate_grads = coords.requires_grad
+        self._need_box_grads = box.requires_grad
         self.coords = coords
         self.box = box
-        self.box_inv = torch.inverse(box)
-        self.box_lengths = torch.diag(box)
+        self.box_inv = torch.inverse(self.box)
+        self.box_lengths = torch.diag(self.box)
         self.neighbor_list = CellList(coords, self.box_lengths, cutoff, max_neighbors=max_neighbors)
         self._get_axis_frame_indices()
+        self._check_for_nl_update = False
 
     def _get_axis_frame_indices(self):
         # TODO: This is only applicable to water. I am not sure exactly how to handle this in general...
@@ -64,13 +67,21 @@ class CoordinateManager:
     def update_coordinates(self, new_coords: torch.Tensor):
         """
         Update coordinates held by the coordinate manager and neighbor list.
-        If neighbor list needs to be rebuilt, it will be.
         """
-        self.coords = new_coords
-        self.coords.requires_grad_()
-        self.neighbor_list.update(new_coords)
+        self.coords = new_coords.detach().clone().requires_grad_()
+        self._check_for_nl_update = True
 
-    def get_distances_vectors_and_pairs(self) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+    def update_box(self, new_box: torch.Tensor):
+        """
+        Update coordinates held by the coordinate manager and neighbor list.
+        """
+        self.box = new_box.detach().clone().requires_grad_()
+        self.box_inv = torch.inverse(self.box)
+        self.box_lengths = torch.diag(self.box)
+        self.neighbor_list.box_lengths = self.box_lengths
+        self._check_for_nl_update = True
+
+    def get_distances_vectors_and_pairs(self, reset_gradients: bool = True) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
         """
         Get all distances, distance vectors, and indices of atom pairs.
         """
@@ -78,6 +89,12 @@ class CoordinateManager:
         # transpose. I suppose it probably doens't matter since I end up using both
         # but if I could avoid that, then that would be ideal. I guess I could
         # transpose the coordinate to solve this problem?
+        
+        if reset_gradients:
+            self.update_coordinates(self.coords)
+            self.update_box(self.box)
+        if self._check_for_nl_update:
+            self.neighbor_list.update(self.coords, self.box_lengths)
         self.pairs = self.neighbor_list.get_pairs()
         distance_vecs = self.coords[self.pairs[:, 1]] - self.coords[self.pairs[:, 0]]
         self.distance_vecs = applyPBC(distance_vecs, self.box, self.box_inv)

cmm/misc_utils.py
@@ -82,7 +82,7 @@ def read_from_tinker_xyz(xyz_file: str, requires_grad=True):
     permutation = np.argsort(bonds[0], kind='stable') # Make sure sort is stable so equivalent indices don't get swapped.
     bonds[0] = bonds[0][permutation]
     bonds[1] = bonds[1][permutation]
-    atom_types = torch.tensor(atom_types, dtype=torch.long) - 1
+    atom_types = torch.tensor(atom_types, dtype=torch.long, requires_grad=False) - 1
     coords = torch.tensor(coords / BOHR2ANG, dtype=torch.float64, requires_grad=requires_grad)
     return coords, atom_types, bonds
 

cmm/neighbor_list.py
@@ -118,8 +118,8 @@ class CellList(NeighborList):
             assert False, "You requested a cutoff that is larger than the smallest box direction. We can't handle this currently. Set the cutoff to the smallest box direction or smaller."
         
         # Find number of cells in each direction then compute all valid cells.
-        self.n_cells = torch.floor(box_lengths / cutoff).long()
-        self.cell_size = box_lengths / self.n_cells
+        self.n_cells = torch.floor(self.box_lengths / cutoff).long()
+        self.cell_size = self.box_lengths / self.n_cells
         
         # Initialize cell assignments
         self.n_atoms = positions.shape[0]
@@ -259,7 +259,7 @@ class CellList(NeighborList):
         max_displacement = torch.max(torch.abs(positions - self.last_positions))
         return max_displacement > 0.5 * torch.min(self.cell_size)
 
-    def update(self, positions: torch.Tensor) -> None:
+    def update(self, positions: torch.Tensor, box_lengths: torch.Tensor) -> None:
         """
         Update cell list with new positions if needed.
         Increments a counter that keeps track of how many updates
@@ -268,7 +268,9 @@ class CellList(NeighborList):
         
         Args:
             positions (torch.Tensor): (N, 3) array of new positions
+            box_lengths (torch.Tensor): (3) array of box lengths
         """
+        self.box_lengths = box_lengths
         # Check if a rebuild of the cells is needed
         if self._needs_rebuild(positions):
             self._build(positions)

cmm/topology.py
@@ -13,7 +13,7 @@ from .neighbor_list import NeighborList
 class Topology:
     def __init__(self, bonds: NDArray[np.int64], nl: NeighborList, natoms: int):
         self.natoms = natoms
-        self.bonded_atoms = torch.tensor(bonds, dtype=torch.long, device=nl.device)
+        self.bonded_atoms = torch.tensor(bonds, dtype=torch.long, device=nl.device, requires_grad=False)
         self.find_bond_and_angle_pair_indices(nl)
 
     def _find_bond_indices_i(self, i: torch.Tensor, pairs_i: torch.Tensor, n_neighbors: torch.Tensor):

tests/test_cmm.py
@@ -146,6 +146,33 @@ def water_data(coords: torch.Tensor):
         param_ct,
     )
 
+def test_multiple_evaluations():
+    torch.set_default_dtype(torch.float64)
+
+    coords, atom_types, bonds = read_from_tinker_xyz(os.path.join(os.path.dirname(__file__), "data/water_dimer.xyz"), requires_grad=True)
+    atom_indices_to_names = {0: "O_water", 1: "H_water"}
+    atom_type_names = [atom_indices_to_names[int(atom_types[i])] for i in range(len(atom_types))]
+
+    box = torch.tensor(np.eye(3) * 100, dtype=torch.float64, requires_grad=True)
+    
+    ff = CMM()
+    cm = CoordinateManager(coords, box, 10.0 / BOHR2ANG, 1024)
+    topology = Topology(bonds, cm.neighbor_list, coords.size(0))
+    pairs, dists, dist_vecs = cm.get_distances_vectors_and_pairs()
+    parameters = Parameterizer(
+        atom_type_names, pairs, topology.angle_atoms,
+        ff.atomic_params, ff.pair_params, ff.pair_pair_params, ff.pair_angle_params, ff.angle_params
+    )
+    energies_ff = ff.evaluate(cm, topology, parameters)
+    energies_ff['tot'].backward()
+    grads_1 = cm.coords.grad.detach().clone()
+
+    energies_ff = ff.evaluate(cm, topology, parameters)
+    energies_ff['tot'].backward()
+    grads_2 = cm.coords.grad.detach().clone()
+    
+    assert torch.allclose(grads_1, grads_2)
+
 def test_fast_evaluate():
     torch.set_default_dtype(torch.float64)
 
@@ -224,8 +251,8 @@ def test_total_energy_and_total_gradients():
     if not torch.allclose(grads_ad_2, grads_fd_2):
         print(grads_ad_2 - grads_fd_2)
 
-    assert torch.allclose(grads_ad_1, grads_fd_1)#, atol=1e-7)
-    assert torch.allclose(grads_ad_2, grads_fd_2)#, atol=1e-7)
+    assert torch.allclose(grads_ad_1, grads_fd_1)
+    assert torch.allclose(grads_ad_2, grads_fd_2)
 
 def test_nonbonded_interactions():
     torch.set_default_dtype(torch.float64)

tests/test_md.py
@@ -22,6 +22,7 @@ def test_virial_tensor():
     atom_type_names = [atom_indices_to_names[int(atom_types[i])] for i in range(len(atom_types))]
     box = torch.tensor(np.eye(3) * 18.643 / BOHR2ANG, dtype=torch.float64, requires_grad=True)
     box_volume = torch.det(box)
+
     cm = CoordinateManager(coords, box, 10.0 / BOHR2ANG, 1024)
     topology = Topology(bonds, cm.neighbor_list, coords.size(0))
     pairs, dists, dist_vecs = cm.get_distances_vectors_and_pairs()
@@ -32,14 +33,15 @@ def test_virial_tensor():
     )
 
     energies = ff.evaluate(cm, topology, parameters)
-    energies['tot'].backward(retain_graph=True)
+    energies['tot'].backward()
 
-    right = torch.matmul(box.grad.T, box)
-    left = torch.matmul(coords.grad.T, coords)
-    virial = right - left
-    stress = virial / box_volume
-    print(virial)
-    print(stress)
+    # This equation for the virial stress is derived in the Appendix of: https://doi.org/10.1016/j.cpc.2019.107057 
+    #right = torch.matmul(box.grad.T, box)
+    #left = torch.matmul(coords.grad.T, coords)
+    #virial = right + left
+    #stress = virial / box_volume
+    #print(virial)
+    #print(stress)
 
 def test_md():
     torch.set_default_dtype(torch.float64)