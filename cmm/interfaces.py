c3441cf29a33a3374b92c73511db5a606d5e661f
heindelj <heindelj@lbl.gov>
2025-01-27 12:44:29 -0800

    Can optimize water dimer but need to expand the tests to other dimers and make an actual comparison. Ensuring water works in PBC now.

--- cmm/interfaces.py
+++ cmm/interfaces.py
@@ -25,6 +25,8 @@ class CMM_ASE(Calculator):
 
         self.implemented_properties = ['energy', 'forces'] # TODO: add stress and dipole
     
+        self._energies = {}
+
         self.results = {'energy': 0.0,
                 'forces': np.zeros((len(self.atoms), 3)),
                 'stress': np.zeros(6),
@@ -33,31 +35,36 @@ class CMM_ASE(Calculator):
                 'magmom': 0.0,
                 'magmoms': np.zeros(len(self.atoms))}
     
-    def _evaluate_ff(self, evaluate_gradients: bool):
-        energies = self._ff.evaluate(self._cm, self._topology, self._params)
-        self.results['energy'] = float(energies['tot'].detach().cpu())
-        if evaluate_gradients:
-            energies['tot'].backward()
-            self.results['forces'] = self._cm.coords.grad.detach().cpu().numpy()
+    def _evaluate_ff(self):
+            self._energies = self._ff.evaluate(self._cm, self._topology, self._params)
+            self._energies['tot'].backward()
 
-    def get_potential_energy(self, atoms=None):
+            self.results['energy'] = float(self._energies['tot'].detach().cpu())
+            self.results['forces'] = -self._cm.coords.grad.detach().cpu().numpy()
+
+    def get_potential_energy(self, atoms=None, force_consistent=False, apply_constraint=True):
         self.calculate(atoms=atoms)
         return self.results['energy']
     
-    def get_forces(self, atoms=None):
-        self.calculate(atoms=atoms)
-        return self.results['forces']
+    #def get_forces(self, atoms=None, apply_constraint=True, md=False):
+    #    return self.results['forces']
 
+    # TODO: ASE does not always populate these properties or system_changes arrays
+    # which makes it difficult to know how to respond to a call to calculate.
+    # Currently I have removed the get_forces method since having this present
+    # just seems to result in calculations happening twice. I should basically
+    # keep track of when we last updated the positions, last evaluated the force
+    # field and last back-propagated through so that I don't have to rely on ASE
+    # to figure out what needs to be calculated. Instead, I can just do the minimal
+    # work required for whatever function ASE is calling.
     def calculate(self,
         atoms=None,
         properties=['energy', 'forces', 'stress', 'dipole'],
         system_changes=[]
     ):
         if atoms:
-            self.atoms = atoms
-        if 'positions' in system_changes:
             # Update positions on GPU #
+            self.atoms = atoms
             self._cm.update_coordinates(torch.from_numpy(self.atoms.positions).to(self._cm.coords.device))
-            print("Positions changed")
         
-        self._evaluate_ff('forces' in properties)
+        self._evaluate_ff()
--- tests/test_interfaces.py
+++ tests/test_interfaces.py
@@ -38,7 +38,7 @@ def test_ase():
     ff_ase = CMM_ASE(ff, cm, topology, parameters)
     atoms = ff_ase.atoms
     atoms.get_forces()
-    
+
     assert torch.isclose(energies['tot'], torch.tensor(ff_ase.results['energy']))
     assert torch.allclose(grad_1, torch.from_numpy(ff_ase.results['forces']))
 
@@ -63,10 +63,33 @@ def test_optimize_dimers_via_ase():
     ff_ase = CMM_ASE(ff, cm, topology, parameters)
     ff_ase.calculate()
     dyn = LBFGS(ff_ase.atoms)
-    dyn.run(fmax=0.00000001)
-    print(ff_ase.atoms.get_potential_energy())
+    dyn.run(fmax=1e-6)
+    
     # TODO:
     # 1) get water dimer, ion-water dimers, and ion-ion dimers
     # 2) optimize all of them with ASE and check we reproduce the structures from Julia
     # 3) In order to achieve above will have to implement variable polarizability and some topology things for ions
     pass
+
+def test_optimize_water_box_via_ase():
+    torch.set_default_dtype(torch.float64)
+
+    coords, atom_types, bonds = read_from_tinker_xyz(os.path.join(os.path.dirname(__file__), "data/water_216.xyz"), requires_grad=True)
+    
+    # Normally, the parser should enforce just returning the names of atom types
+    atom_indices_to_names = {0: "O_water", 1: "H_water"}
+    atom_type_names = [atom_indices_to_names[int(atom_types[i])] for i in range(len(atom_types))]
+    box = torch.tensor(np.eye(3) * 18.643 / BOHR2ANG, dtype=torch.float64, requires_grad=True)
+    cm = CoordinateManager(coords, box, 10.0 / BOHR2ANG, 1024)
+    topology = Topology(bonds, cm.neighbor_list, coords.size(0))
+    pairs, dists, dist_vecs = cm.get_distances_vectors_and_pairs()
+    ff = CMM()
+    parameters = Parameterizer(
+        atom_type_names, pairs, topology.angle_atoms,
+        ff.atomic_params, ff.pair_params, ff.pair_pair_params, ff.pair_angle_params, ff.angle_params
+    )
+    
+    ff_ase = CMM_ASE(ff, cm, topology, parameters)
+    ff_ase.calculate()
+    dyn = LBFGS(ff_ase.atoms)
+    dyn.run(fmax=1e-6)
