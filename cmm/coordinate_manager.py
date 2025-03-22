Energies and gradients involving all fields and induced fields are correct, if you allow tracking through the polarization solve.
For now, I have turned it off since its horribly slow to track gradients in that way. Something is still broken with MD...

cmm/coordinate_manager.py
@@ -5,7 +5,7 @@ from .pbc import applyPBC
 from .axis_types import AxisTypes
 
 class CoordinateManager:
-    def __init__(self, coords: torch.Tensor, box: torch.Tensor, cutoff: float, labels: Optional[List[str]]=None, max_neighbors: int = 512) -> None:
+    def __init__(self, coords: torch.Tensor, box: torch.Tensor, cutoff: float, labels: List[str], max_neighbors: int = 512) -> None:
         self._need_coordindate_grads = coords.requires_grad
         self._need_box_grads = box.requires_grad
         self.coords = coords
@@ -35,7 +35,7 @@ class CoordinateManager:
         self.neighbor_list.box_lengths = self.box_lengths
         self._check_for_nl_update = True
 
-    def get_distances_vectors_and_pairs(self, reset_gradients: bool = False) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+    def get_distances_vectors_and_pairs(self) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
         """
         Get all distances, distance vectors, and indices of atom pairs.
         """
@@ -44,13 +44,22 @@ class CoordinateManager:
         # but if I could avoid that, then that would be ideal. I guess I could
         # transpose the coordinate to solve this problem?
         
-        if reset_gradients:
+        # If we have nonzero grads, but we are here, then we are evaluating the force field
+        # again without having used the gradients. Currently, this can only because we
+        # did not actually do the backwards pass. In that case, we should just reset
+        # the gradients since accumulating into them will definitely be incorrect.
+        # In the future, we will a more sophisticated method than this since evaluating
+        # over batches of pairs would have us call this function multiple times with the
+        # intention of accumulating gradients to the coordinates multiple times before
+        # calling .backward().
+        if self.coords.grad is not None:
             self.update_coordinates(self.coords)
             self.update_box(self.box)
+
         # TODO: Something is broken about NL rebuilds!
         # Fix that!
-        #if self._check_for_nl_update:
-        #    self.neighbor_list.update(self.coords, self.box_lengths)
+        if self._check_for_nl_update:
+            self.neighbor_list.update(self.coords, self.box_lengths)
         with torch.no_grad():
             self.pairs = self.neighbor_list.get_pairs()
         distance_vecs = self.coords[self.pairs[:, 1]] - self.coords[self.pairs[:, 0]]
@@ -139,9 +148,9 @@ class CoordinateManager:
         # No axis
         filterNoAxis = (axis_types == AxisTypes.NoAxisType.value)
         if torch.any(filterNoAxis):
-            xVecNoAxis = torch.tensor([1.0, 0.0, 0.0])
-            yVecNoAxis = torch.tensor([0.0, 1.0, 0.0])
-            zVecNoAxis = torch.tensor([0.0, 0.0, 1.0])
+            xVecNoAxis = torch.tensor([1.0, 0.0, 0.0], device=zVec.device)
+            yVecNoAxis = torch.tensor([0.0, 1.0, 0.0], device=zVec.device)
+            zVecNoAxis = torch.tensor([0.0, 0.0, 1.0], device=zVec.device)
             xVec[filterNoAxis] = xVec[filterNoAxis] + xVecNoAxis
             yVec[filterNoAxis] = yVec[filterNoAxis] + yVecNoAxis
             zVec[filterNoAxis] = zVec[filterNoAxis] + zVecNoAxis

cmm/force_field.py
@@ -367,9 +367,9 @@ class CMM(ForceField):
             self.parameters_have_changed = True
 
     #@torch.compile
-    def evaluate(self, cm: CoordinateManager, topology: Topology, params: Parameterizer, reset_gradients: bool=True):
+    def evaluate(self, cm: CoordinateManager, topology: Topology, params: Parameterizer):
         # Get all intermolecular and intramolecular pairs, dists, and vectors inside long-range cutoff #
-        pairs, dists, dist_vecs = cm.get_distances_vectors_and_pairs(reset_gradients=reset_gradients)
+        pairs, dists, dist_vecs = cm.get_distances_vectors_and_pairs()
         
         if self.parameters_have_changed:
             # SPEED: Can of course do this per parameter type so that not everything is rebuilt
@@ -607,8 +607,9 @@ class CMM(ForceField):
         multipoles_real = convertMultipolesToPolytensor(
             mono_lr, dipo_lr, quad_lr
         )
-        multipoles_cp = multipoles_real.clone()
-        multipoles_cp[:, 0] = mono - Z
+        multipoles_cp = convertMultipolesToPolytensor(
+            mono_lr - Z, dipo_lr, quad_lr
+        )
 
         # @SPEED: Z_mpoles is all zeros besides the charge. Can certainly avoid allocating the
         # multipolar array entries and thereby eliminate the multiplications by zero.
@@ -672,16 +673,16 @@ class CMM(ForceField):
         # Accumulate the total potentials, fields, and field gradients.
         # One contribution is accumulated over all long-range pairs, while the other
         # accumulates just the penetration contribution.
-        all_field_data = torch.zeros(natoms, 10, device=dists.device, dtype=dists.dtype)
-        all_field_data.scatter_add_(0, pairs_lr_j_a.unsqueeze(1).expand(-1, 10), edata_point_pairwise.squeeze(2))
-        all_field_data.scatter_add_(0, pairs_sr_j_a.unsqueeze(1).expand(-1, 10), edata_cs_pairwise_ij.squeeze(2))
+        all_field_data = torch.zeros(natoms, 10, device=dists.device, dtype=dists.dtype, requires_grad=True)
+        all_field_data = all_field_data.scatter_add(0, pairs_lr_j_a.unsqueeze(1).expand(-1, 10), edata_point_pairwise.squeeze(2))
+        all_field_data = all_field_data.scatter_add(0, pairs_sr_j_a.unsqueeze(1).expand(-1, 10), edata_cs_pairwise_ij.squeeze(2))
 
         if self.use_ewald:
             edata_point_excl_pairwise = torch.bmm(direct_field_tensor_excl, multipoles_excl_i_p.unsqueeze(2))
             elec_point_excl_pairwise = torch.bmm(multipoles_excl_j_p.unsqueeze(1), edata_point_excl_pairwise).flatten()
-            all_field_data.scatter_add_(0, pairs_excl_j_a.unsqueeze(1).expand(-1, 10), edata_point_excl_pairwise.squeeze(2))
+            all_field_data = all_field_data.scatter_add(0, pairs_excl_j_a.unsqueeze(1).expand(-1, 10), edata_point_excl_pairwise.squeeze(2))
 
-        all_field_data.mul_(torch.tensor([1, -1, -1, -1, -1, -1, -1, -1, -1, -1], device=pairs.device).reshape(1, -1))
+        all_field_data = all_field_data.mul(torch.tensor([1, -1, -1, -1, -1, -1, -1, -1, -1, -1], device=pairs.device).reshape(1, -1))
         elec_potential = all_field_data[:, 0]
         elec_field = all_field_data[:, 1:4]
         if self.use_ewald:
@@ -703,8 +704,8 @@ class CMM(ForceField):
         dq_forward = multipoles_ct_don_i_p[:, 0] * multipoles_ct_acc_j_p[:, 0] * drInvDamp_ct * eps
         dq_backward = multipoles_ct_acc_i_p[:, 0] * multipoles_ct_don_j_p[:, 0] * drInvDamp_ct * eps
         dq_pairwise = (dq_forward - dq_backward) * switch_sr
-        dq_a = torch.zeros(natoms, device=pairs.device)
-        dq_groups = torch.zeros(topology.n_pol_groups, device=pairs.device)
+        dq_a = torch.zeros(natoms, device=pairs.device, requires_grad=True)
+        dq_groups = torch.zeros(topology.n_pol_groups, device=pairs.device, requires_grad=True)
         dq_a = dq_a.scatter_add(0, pairs_sr_j_a, dq_pairwise)
         dq_groups = segment_csr(dq_a[topology.pol_group_indices_a], topology.pol_group_segment_indices, reduce='sum')
 
@@ -713,15 +714,19 @@ class CMM(ForceField):
         inverse_polarizabilities = torch.linalg.inv(polarizabilities)
 
         b_vec = torch.hstack((-elec_potential, elec_field.flatten(), dq_groups))
-        induced_field_data = torch.zeros(natoms, 4, device=dists.device, dtype=dists.dtype)
         
         long_range_induced_potential_function = None
         if self.use_polarization:
+            #induced_field_data = torch.zeros(natoms, 4, device=dists.device, dtype=dists.dtype, requires_grad=True)
             if self.use_ewald:
                 long_range_induced_potential_function = lambda charges, dipoles : long_range_potential_rank_1(cm.coords, charges, dipoles, cm.box, self.alpha_ewald, self.k_max)
+            
+            self.last_induced_multipoles = None
+            #self.last_induced_multipoles = torch.zeros(natoms + 3 * natoms + topology.n_pol_groups, device=elec_field.device, requires_grad=True)
             with torch.no_grad():
+            #with torch.enable_grad():
                 if self.last_induced_multipoles is None:
-                    self.last_induced_multipoles = direct_field_induced_dipole_guess(natoms, natoms, topology.n_pol_groups, polarizabilities, elec_field)
+                    self.last_induced_multipoles = direct_field_induced_dipole_guess(natoms, topology.n_pol_groups, polarizabilities, elec_field)
 
                 induced_multipoles_and_lagrange_muls_out = solvePolarizationByCG(
                     self.last_induced_multipoles,
@@ -731,7 +736,7 @@ class CMM(ForceField):
                     pairs_sr_i_a, pairs_sr_j_a,
                     pairs_excl_i_a, pairs_excl_j_a,
                     direct_field_tensor_rank_1_lr, pol_interaction_tensor_sr, direct_field_tensor_excl_rank_1,
-                    induced_field_data,
+                    #induced_field_data,
                     eta_times_2, inverse_polarizabilities,
                     topology.pol_group_indices_a,
                     topology.pol_group_segment_indices,
@@ -741,19 +746,19 @@ class CMM(ForceField):
                 self.last_induced_multipoles = induced_multipoles_and_lagrange_muls_out
 
             TM, elec_potential_induced, elec_field_induced  = computeProductWithPolarizationMatrix(
-                induced_multipoles_and_lagrange_muls_out, natoms,
+                self.last_induced_multipoles, natoms,
                 pairs_lr_i_a, pairs_lr_j_a,
                 pairs_sr_i_a, pairs_sr_j_a,
                 pairs_excl_i_a, pairs_excl_j_a,
                 direct_field_tensor_rank_1_lr, pol_interaction_tensor_sr, direct_field_tensor_excl_rank_1,
-                induced_field_data,
+                #induced_field_data,
                 eta_times_2, inverse_polarizabilities,
                 topology.pol_group_indices_a,
                 topology.pol_group_segment_indices,
                 topology.pol_group_lengths_g,
                 long_range_induced_potential_function
             )
-            ene_pol = torch.dot(induced_multipoles_and_lagrange_muls_out, (0.5 * TM - b_vec))
+            ene_pol = torch.dot(self.last_induced_multipoles, (0.5 * TM - b_vec))
         else:
             ene_pol = torch.tensor(0.0)
 

cmm/interfaces.py
@@ -45,7 +45,7 @@ class CMM_ASE(Calculator):
                 'magmom': 0.0,
                 'magmoms': np.zeros(len(self.atoms))}
         
-        self.calculate() # Calculate at beginning to seed forces
+        #self.calculate(self.atoms) # Calculate at beginning to seed forces
     
     def save_state(self, filename: str):
         """
@@ -213,14 +213,6 @@ class CMM_ASE(Calculator):
             topo_filename = full_path + '.topology.json'
             self.save_state(topo_filename)
 
-    def _evaluate_ff(self):
-            self._energies = self._ff.evaluate(self._cm, self._topology, self._params)
-            self._energies['tot'].backward()
-
-            self.results['energy'] = float(self._energies['tot'].detach().cpu()) * Hartree
-            print(self.results['energy'])
-            self.results['forces'] = -self._cm.coords.grad.detach().cpu().numpy() * (Hartree / Bohr)
-
     def get_potential_energy(self, atoms=None, force_consistent=False, apply_constraint=True):
         self.calculate(atoms=atoms)
         return self.results['energy']
@@ -244,6 +236,10 @@ class CMM_ASE(Calculator):
         if atoms:
             # Update positions on GPU #
             self.atoms = atoms
-            self._cm.update_coordinates(torch.from_numpy(self.atoms.positions / Bohr).to(self._cm.coords.device))
-        
-        self._evaluate_ff()
+        self._cm.update_coordinates(torch.from_numpy(self.atoms.positions / Bohr).to(self._cm.coords.device))
+
+        self._energies = self._ff.evaluate(self._cm, self._topology, self._params)
+        self._energies['tot'].backward()
+
+        self.results['energy'] = float(self._energies['tot'].detach().cpu()) * Hartree
+        self.results['forces'] = -self._cm.coords.grad.detach().cpu().numpy() * (Hartree / Bohr)

cmm/polarization.py
@@ -16,14 +16,17 @@ def get_field_dependent_polarizabilities(
 
 def direct_field_induced_dipole_guess(
     n_charges: torch.NumberType,
-    n_dipoles: torch.NumberType,
     n_groups: torch.NumberType,
     polarizabilities: torch.Tensor,
     elec_field: torch.Tensor
 ):
-    guess_vector = torch.zeros(n_charges + 3 * n_dipoles + n_groups, device=elec_field.device)
-    guess_vector[n_charges:(n_charges + 3 * n_dipoles)] = torch.bmm(polarizabilities, elec_field.unsqueeze(-1)).squeeze(-1).flatten()
-    return guess_vector
+    dipole_part = torch.bmm(polarizabilities, elec_field.unsqueeze(-1)).squeeze(-1).flatten()
+
+    return torch.cat([
+        torch.zeros(n_charges, device=elec_field.device),
+        dipole_part,
+        torch.zeros(n_groups, device=elec_field.device)
+    ])
 
 def solvePolarizationByCG(
     guess_vector: torch.Tensor,
@@ -35,7 +38,7 @@ def solvePolarizationByCG(
     direct_field_tensor_lr: torch.Tensor,
     pol_interaction_tensor_sr: torch.Tensor,
     direct_field_tensor_excl: torch.Tensor,
-    induced_field_data: torch.Tensor,
+    #induced_field_data: torch.Tensor,
     eta: torch.Tensor,
     inverse_polarizabilities: torch.Tensor,
     pol_group_indices_a: torch.Tensor,
@@ -58,7 +61,7 @@ def solvePolarizationByCG(
         pairs_sr_i_a, pairs_sr_j_a,
         pairs_excl_i_a, pairs_excl_j_a,
         direct_field_tensor_lr, pol_interaction_tensor_sr, direct_field_tensor_excl,
-        induced_field_data,
+        #induced_field_data,
         eta, inverse_polarizabilities,
         pol_group_indices_a,
         pol_group_segment_indices,
@@ -66,7 +69,8 @@ def solvePolarizationByCG(
         long_range_potential_function
     )
     residual = b_vector - TM0
-    P = residual.detach().clone()
+    #P = residual.detach().clone()
+    P = residual.clone()
     for i_iter in range(max_iter):
         TP, _, _ = computeProductWithPolarizationMatrix(
             P, n_charges,
@@ -74,7 +78,7 @@ def solvePolarizationByCG(
             pairs_sr_i_a, pairs_sr_j_a,
             pairs_excl_i_a, pairs_excl_j_a,
             direct_field_tensor_lr, pol_interaction_tensor_sr, direct_field_tensor_excl,
-            induced_field_data,
+            #induced_field_data,
             eta, inverse_polarizabilities,
             pol_group_indices_a,
             pol_group_segment_indices,
@@ -82,13 +86,13 @@ def solvePolarizationByCG(
             long_range_potential_function
         )
         gamma = torch.dot(residual, residual) / torch.dot(P, TP)
-        guess_vector += gamma * P
+        guess_vector = guess_vector + gamma * P
         beta = 1.0 / torch.dot(residual, residual)
-        residual -= gamma * TP
+        residual = residual - gamma * TP
         if torch.norm(residual) < residual_threshold:
             #print(i_iter, " steps to converge")
             return guess_vector
-        beta *= torch.dot(residual, residual)
+        beta = beta * torch.dot(residual, residual)
         P = residual + beta * P
     return guess_vector
 
@@ -101,7 +105,7 @@ def computeProductWithPolarizationMatrix(
     direct_field_tensor_lr: torch.Tensor,
     pol_interaction_tensor_sr: torch.Tensor,
     direct_field_tensor_excl: torch.Tensor,
-    induced_field_data: torch.Tensor,
+    #induced_field_data: torch.Tensor,
     eta: torch.Tensor,
     alpha_inv: torch.Tensor,
     pol_group_indices_a: torch.Tensor,
@@ -124,16 +128,21 @@ def computeProductWithPolarizationMatrix(
     # Accumulate the total potentials, fields, and field gradients.
     # One contribution is accumulated over all long-range pairs, while the other
     # accumulates just the penetration contribution.
-    induced_field_data = torch.zeros_like(induced_field_data) # <-- I think this avoids the allocation
-    induced_field_data.scatter_add_(0, pairs_lr_j_a.unsqueeze(1).expand(-1, 4), edata_point_pairwise.squeeze(2))
-    induced_field_data.scatter_add_(0, pairs_sr_j_a.unsqueeze(1).expand(-1, 4), edata_ss_pairwise.squeeze(2))
+    #induced_field_data = torch.zeros_like(induced_field_data) # <-- I think this avoids the allocation
+    induced_field_data = torch.zeros(n_charges, 4, device=induced_multipoles_a.device, dtype=induced_multipoles_a.dtype, requires_grad=True)
+    #induced_field_data.scatter_add_(0, pairs_lr_j_a.unsqueeze(1).expand(-1, 4), edata_point_pairwise.squeeze(2))
+    induced_field_data = induced_field_data.scatter_add(0, pairs_lr_j_a.unsqueeze(1).expand(-1, 4), edata_point_pairwise.squeeze(2))
+    #induced_field_data.scatter_add_(0, pairs_sr_j_a.unsqueeze(1).expand(-1, 4), edata_ss_pairwise.squeeze(2))
+    induced_field_data = induced_field_data.scatter_add(0, pairs_sr_j_a.unsqueeze(1).expand(-1, 4), edata_ss_pairwise.squeeze(2))
     
     if long_range_potential_function:
         induced_multipoles_i_excl_p = induced_multipoles_a[pairs_excl_i_a]
         edata_point_excl_pairwise = torch.bmm(direct_field_tensor_excl, induced_multipoles_i_excl_p.unsqueeze(2))
-        induced_field_data.scatter_add_(0, pairs_excl_j_a.unsqueeze(1).expand(-1, 4), edata_point_excl_pairwise.squeeze(2))
+        #induced_field_data.scatter_add_(0, pairs_excl_j_a.unsqueeze(1).expand(-1, 4), edata_point_excl_pairwise.squeeze(2))
+        induced_field_data = induced_field_data.scatter_add(0, pairs_excl_j_a.unsqueeze(1).expand(-1, 4), edata_point_excl_pairwise.squeeze(2))
     
-    induced_field_data.mul_(torch.tensor([1, -1, -1, -1], device=pairs_lr_i_a.device).reshape(1, -1))
+    #induced_field_data.mul_(torch.tensor([1, -1, -1, -1], device=pairs_lr_i_a.device).reshape(1, -1))
+    induced_field_data = induced_field_data.mul(torch.tensor([1, -1, -1, -1], device=pairs_lr_i_a.device).reshape(1, -1))
     induced_electric_potential = induced_field_data[:, 0]
     induced_electric_field = induced_field_data[:, 1:4]
     # Get reciprocal space field data (ewald + self contribution) #
@@ -151,8 +160,9 @@ def computeProductWithPolarizationMatrix(
     expanded_lagrange_muls = lagrange_muls.repeat_interleave(pol_group_lengths_g)
 
     # Now we need to scatter these values back to the atomic indices
-    lagrange_muls_a = torch.zeros(n_charges, device=lagrange_muls.device)
-    lagrange_muls_a.scatter_add_(0, pol_group_indices_a, expanded_lagrange_muls)
+    lagrange_muls_a = torch.zeros(n_charges, device=lagrange_muls.device, requires_grad=True)
+    #lagrange_muls_a.scatter_add_(0, pol_group_indices_a, expanded_lagrange_muls)
+    lagrange_muls_a = lagrange_muls_a.scatter_add(0, pol_group_indices_a, expanded_lagrange_muls)
 
     res = torch.concat((
         eta * induced_charges + lagrange_muls_a + induced_electric_potential,

tests/test_cmm.py
@@ -216,9 +216,11 @@ def test_total_energy_and_total_gradients_ion_ion():
 
 def test_total_energy_and_total_gradients_ion_water():
     torch.set_default_dtype(torch.float64)
-
-    coords_no_grad, _, _, _ = read_from_tinker_xyz(os.path.join(os.path.dirname(__file__), "data/h2o_f.xyz"), requires_grad=False)
-    coords, atom_types, bonds, _ = read_from_tinker_xyz(os.path.join(os.path.dirname(__file__), "data/h2o_f.xyz"), requires_grad=True)
+    
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    coords_no_grad, _, _, _ = read_from_tinker_xyz(os.path.join(os.path.dirname(__file__), "data/h2o_f.xyz"), requires_grad=False, device=device)
+    coords, atom_types, bonds, labels = read_from_tinker_xyz(os.path.join(os.path.dirname(__file__), "data/h2o_f.xyz"), requires_grad=True, device=device)
+    
     #coords_no_grad, _, _, _ = read_from_tinker_xyz(os.path.join(os.path.dirname(__file__), "data/w5_f.xyz"), requires_grad=False)
     #coords, atom_types, bonds, _ = read_from_tinker_xyz(os.path.join(os.path.dirname(__file__), "data/w5_f.xyz"), requires_grad=True)
     atom_indices_to_names = {
@@ -227,9 +229,9 @@ def test_total_energy_and_total_gradients_ion_water():
         6: "Li+", 7: "Na+", 8: "K+", 9: "Rb+", 10: "Cs+"
     }
     atom_type_names = [atom_indices_to_names[int(atom_types[i])] for i in range(len(atom_types))]
-    box = torch.tensor(np.eye(3) * 100, dtype=torch.float64, requires_grad=False)
+    box = torch.tensor(np.eye(3) * 100, dtype=torch.float64, requires_grad=False, device=device)
     
-    cm = CoordinateManager(coords, box, 10.0 / BOHR2ANG, 1024)
+    cm = CoordinateManager(coords, box, 10.0 / BOHR2ANG, labels, 1024)
     topology = Topology(bonds, cm.neighbor_list, coords.size(0))
     pairs, _, _ = cm.get_distances_vectors_and_pairs()
     ff = CMM(cutoff_short_range=torch.tensor(10.0 / BOHR2ANG))
@@ -239,29 +241,31 @@ def test_total_energy_and_total_gradients_ion_water():
     )
 
     energies_ff = ff.evaluate(cm, topology, parameters)
-    total_ref = torch.tensor([-28.231218043296266 / HARTREE2KCAL])
+    total_ref = torch.tensor([-28.231218043296266], device=device)
+    total_energy = energies_ff['tot'] * HARTREE2KCAL
     # NOTE(JOE): REQUIRES NONZERO INDUCED FIELD FOR FD MORSE TO PASS. FIX THE GRADIENT BUG BY WRITING CUSTOM BACKWARD METHOD FOR ENE_POL?
-    assert torch.allclose(energies_ff['tot'], total_ref)
+    assert torch.allclose(total_energy, total_ref)
 
-    #def get_total_energy(coords: torch.Tensor):
-    #    cm = CoordinateManager(coords, box, 10.0 / BOHR2ANG, 1024)
-    #    topology = Topology(bonds, cm.neighbor_list, coords.size(0))
-    #    pairs, _, _ = cm.get_distances_vectors_and_pairs()
-    #    ff = CMM()
-    #    parameters = Parameterizer(
-    #        atom_type_names, pairs, topology.angle_atoms,
-    #        ff.atomic_params, ff.pair_params, ff.pair_pair_params, ff.pair_angle_params, ff.angle_params
-    #    )
-    #    energies_ff = ff.evaluate(cm, topology, parameters)
-    #    total_energy = energies_ff['tot']
-    #    return total_energy
-#
-    #grads_fd_1 = finite_difference(coords_no_grad, get_total_energy, h=1e-5)
-    #energies_ff['tot'].backward(retain_graph=True)
-    #grads_ad_1 = coords.grad.clone()
-    #if not torch.allclose(grads_ad_1, grads_fd_1):
-    #    print(grads_ad_1 - grads_fd_1)
-    #assert torch.allclose(grads_ad_1, grads_fd_1)
+    energies_ff['tot'].backward()
+    grads_ad_1 = coords.grad.clone()
+
+    def get_total_energy(coords: torch.Tensor):
+        cm = CoordinateManager(coords, box, 10.0 / BOHR2ANG, labels, 1024)
+        topology = Topology(bonds, cm.neighbor_list, coords.size(0))
+        pairs, _, _ = cm.get_distances_vectors_and_pairs()
+        ff = CMM(cutoff_short_range=torch.tensor(10.0 / BOHR2ANG))
+        parameters = Parameterizer(
+            atom_type_names, pairs, topology.angle_atoms,
+            ff.atomic_params, ff.pair_params, ff.pair_pair_params, ff.pair_angle_params, ff.angle_params
+        )
+        energies_ff = ff.evaluate(cm, topology, parameters)
+        total_energy = energies_ff['tot']
+        return total_energy
+
+    grads_fd_1 = finite_difference(coords_no_grad, get_total_energy, h=1e-5).to(device)
+    if not torch.allclose(grads_ad_1, grads_fd_1):
+        print(grads_ad_1 - grads_fd_1)
+    assert torch.allclose(grads_ad_1, grads_fd_1)
 
 def test_total_energy_and_total_gradients():
     torch.set_default_dtype(torch.float64)
@@ -281,10 +285,8 @@ def test_total_energy_and_total_gradients():
         ff.atomic_params, ff.pair_params, ff.pair_pair_params, ff.pair_angle_params, ff.angle_params
     )
 
-    energies_ff = ff.evaluate(cm, topology, parameters, reset_gradients=False)
+    energies_ff = ff.evaluate(cm, topology, parameters)
     total_ref = torch.tensor([-4.768231511534177])
-    #for key in energies_ff.keys():
-    #    print(key, " ", energies_ff[key] * HARTREE2KCAL)
     total_ff = energies_ff['tot'] * HARTREE2KCAL
     assert torch.allclose(total_ff, total_ref)
 

tests/test_interfaces.py
@@ -17,7 +17,7 @@ from cmm.interfaces import CMM_ASE
 
 from ase.units import kcal, mol, Hartree, Bohr, Angstrom
 
-def test_ase():
+def test_ase_basic():
     torch.set_default_dtype(torch.float64)
 
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -26,11 +26,11 @@ def test_ase():
     # Normally, the parser should enforce just returning the names of atom types
     atom_indices_to_names = {0: "O_water", 1: "H_water"}
     atom_type_names = [atom_indices_to_names[int(atom_types[i])] for i in range(len(atom_types))]
-    box = torch.tensor(np.eye(3) * 18.643 / BOHR2ANG, dtype=torch.float64, requires_grad=True, device=device)
+    box = torch.tensor(np.eye(3) * 18.643 / BOHR2ANG, dtype=torch.float64, requires_grad=False, device=device)
     cm = CoordinateManager(coords, box, 10.0 / BOHR2ANG, labels=labels, max_neighbors=1024)
     topology = Topology(bonds, cm.neighbor_list, coords.size(0))
     pairs, dists, dist_vecs = cm.get_distances_vectors_and_pairs()
-    ff = CMM()
+    ff = CMM(use_ewald=True)
     parameters = Parameterizer(
         atom_type_names, pairs, topology.angle_atoms,
         ff.atomic_params, ff.pair_params, ff.pair_pair_params, ff.pair_angle_params, ff.angle_params
@@ -40,12 +40,20 @@ def test_ase():
     energies['tot'].backward()
     grad_1 = cm.coords.grad.detach().clone().cpu()
 
-    ff_ase = CMM_ASE(ff, cm, topology, parameters)
-    atoms = ff_ase.atoms
-    atoms.get_forces()
+    ff_ase = CMM_ASE(CMM(use_ewald=True), cm, topology, parameters)
+    ff_ase.calculate(ff_ase.atoms)
+    print(energies)
+    print(ff_ase._energies)
 
-    assert torch.isclose(energies['tot'], torch.tensor(ff_ase.results['energy']))
-    assert torch.allclose(grad_1, torch.from_numpy(ff_ase.results['forces']))
+    forces_ref = -grad_1 * (Hartree / Bohr)
+    print(forces_ref)
+    print(torch.from_numpy(ff_ase.results['forces']))
+    print(torch.from_numpy(ff_ase.results['forces']) - forces_ref)
+    print(torch.linalg.norm((torch.from_numpy(ff_ase.results['forces']) - forces_ref), dim=1))
+
+
+    assert torch.isclose(energies['tot'] * Hartree, torch.tensor(ff_ase.results['energy']))
+    assert torch.allclose(forces_ref, torch.from_numpy(ff_ase.results['forces']))
 
 def test_cmm_ase_checkpointing():
     torch.set_default_dtype(torch.float64)
@@ -59,10 +67,10 @@ def test_cmm_ase_checkpointing():
     # Normally, the parser should enforce just returning the names of atom types
     atom_indices_to_names = {0: "O_water", 1: "H_water"}
     atom_type_names = [atom_indices_to_names[int(atom_types[i])] for i in range(len(atom_types))]
-    box = torch.tensor(np.eye(3) * 18.643 / BOHR2ANG, dtype=torch.float64, requires_grad=True, device=device)
+    box = torch.tensor(np.eye(3) * 100.0 / BOHR2ANG, dtype=torch.float64, requires_grad=False, device=device)
     cm = CoordinateManager(coords, box, 10.0 / BOHR2ANG, labels=labels, max_neighbors=1024)
     pairs, _, _ = cm.get_distances_vectors_and_pairs()
-    ff = CMM(use_ewald=False)
+    ff = CMM(use_ewald=False, cutoff_short_range=9.0 / BOHR2ANG)
     with torch.no_grad():
         topology = Topology(bonds, cm.neighbor_list, coords.size(0))
         parameters = Parameterizer(
@@ -73,14 +81,21 @@ def test_cmm_ase_checkpointing():
     calculator = CMM_ASE(ff, cm, topology, parameters, output_folder=os.path.join(os.path.dirname(__file__), "scratch"))
     calculator.atoms.calc = calculator
 
-    temperature = 300.0  # K
-    MaxwellBoltzmannDistribution(calculator.atoms, temperature_K=temperature)
+    temperature = 150.0  # K
+    MaxwellBoltzmannDistribution(calculator.atoms, temperature_K=temperature, force_temp=True)
     Stationary(calculator.atoms)
     ZeroRotation(calculator.atoms)
 
+    def log_step(atoms=calculator.atoms):
+        energy = atoms.get_potential_energy()
+        kinetic = atoms.get_kinetic_energy()
+        temperature = atoms.get_temperature()
+        print(f"Step: {dyn.nsteps}, E_pot: {energy:.6f} eV, E_kin: {kinetic:.6f} eV, T: {temperature:.1f} K")
+
     dyn = VelocityVerlet(calculator.atoms, 0.5 * fs, trajectory=os.path.join(os.path.dirname(__file__), 'scratch/w2_dynamics.traj'))
+    dyn.attach(log_step, interval=1)  # Log at every step
     dyn.attach(calculator.create_checkpoint, interval=5)  # Checkpoint every 5 steps
-    finished = dyn.run(4)
+    finished = dyn.run(200)
     if finished:
         calculator.save_state(os.path.join(os.path.dirname(__file__), 'scratch/final_state.json'))
 
@@ -91,23 +106,21 @@ def test_cmm_ase_checkpointing():
 def test_optimize_dimers_via_ase():
     torch.set_default_dtype(torch.float64)
 
-    coords, atom_types, bonds, _ = read_from_tinker_xyz(os.path.join(os.path.dirname(__file__), "data/water_dimer.xyz"), requires_grad=True)
+    coords, atom_types, bonds, labels = read_from_tinker_xyz(os.path.join(os.path.dirname(__file__), "data/water_dimer.xyz"), requires_grad=True)
     atom_indices_to_names = {0: "O_water", 1: "H_water"}
     atom_type_names = [atom_indices_to_names[int(atom_types[i])] for i in range(len(atom_types))]
 
     box = torch.tensor(np.eye(3) * 100, dtype=torch.float64, requires_grad=False)
     
-    cm = CoordinateManager(coords, box, 10.0 / BOHR2ANG, 1024)
+    cm = CoordinateManager(coords, box, 10.0 / BOHR2ANG, labels=labels, max_neighbors=1024)
+    topology = Topology(bonds, cm.neighbor_list, coords.size(0))
     pairs, dists, dist_vecs = cm.get_distances_vectors_and_pairs()
-    ff = CMM(use_ewald=False)
-    with torch.no_grad():
-        topology = Topology(bonds, cm.neighbor_list, coords.size(0))
-        parameters = Parameterizer(
-            atom_type_names, pairs, topology.angle_atoms,
-            ff.atomic_params, ff.pair_params, ff.pair_pair_params, ff.pair_angle_params, ff.angle_params
-        )
+    ff = CMM()
+    parameters = Parameterizer(
+        atom_type_names, pairs, topology.angle_atoms,
+        ff.atomic_params, ff.pair_params, ff.pair_pair_params, ff.pair_angle_params, ff.angle_params
+    )
     ff_ase = CMM_ASE(ff, cm, topology, parameters)
-    ff_ase.calculate()
     dyn = LBFGS(ff_ase.atoms)
     dyn.run(fmax=1e-6)
 
@@ -130,7 +143,7 @@ def test_optimize_water_box_via_ase():
     torch.set_default_dtype(torch.float64)
 
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    coords, atom_types, bonds, labels = read_from_tinker_xyz(os.path.join(os.path.dirname(__file__), "data/water_216.xyz"), device=device)
+    coords, atom_types, bonds, labels = read_from_tinker_xyz(os.path.join(os.path.dirname(__file__), "data/water_216.xyz"), device=device, requires_grad=True)
     permutation = np.argsort(bonds[0], kind='stable') # Make sure sort is stable so equivalent indices don't get swapped.
     bonds[0] = bonds[0][permutation]
     bonds[1] = bonds[1][permutation]
@@ -138,7 +151,7 @@ def test_optimize_water_box_via_ase():
     # Normally, the parser should enforce just returning the names of atom types
     atom_indices_to_names = {0: "O_water", 1: "H_water"}
     atom_type_names = [atom_indices_to_names[int(atom_types[i])] for i in range(len(atom_types))]
-    box = torch.tensor(np.eye(3) * 18.643 / BOHR2ANG, dtype=torch.float64, requires_grad=True, device=device)
+    box = torch.tensor(np.eye(3) * 18.643 / BOHR2ANG, dtype=torch.float64, requires_grad=False, device=device)
     cm = CoordinateManager(coords, box, 10.0 / BOHR2ANG, labels=labels, max_neighbors=1024)
     pairs, dists, dist_vecs = cm.get_distances_vectors_and_pairs()
     ff = CMM(use_ewald=True)