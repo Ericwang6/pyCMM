fa0520efa20392a09de05aeff1941611c51770af
heindelj <heindelj@lbl.gov>
2025-05-22 13:53:02 -0700

    implemented variable hardness

--- cmm/force_fields/cmm.py
+++ cmm/force_fields/cmm.py
@@ -31,6 +31,7 @@ class CMM2(FF):
         self.add_term(StoreMultipolesCMM())
         self.add_term(StoreInteractionTensorsCMM())
         self.add_term(StoreSwitchingValues())
+        self.add_term(VariableHardness())
         self.add_term(TTDispersionC6(lr_disp_settings.use_switching, lr_disp_settings.switching_start_before_cutoff))
         self.add_term(ExchangePolarizationCMM())
         self.add_term(MultipolarElectrostatics2())
--- cmm/terms/parameter/__init__.py
+++ cmm/terms/parameter/__init__.py
@@ -4,3 +4,4 @@ from .store_multipoles_cmm import *
 from .store_interaction_tensors_cmm import *
 from .store_switching_values import StoreSwitchingValues
 from .charge_transfer_cmm import ManyBodyChargeTransfer
+from .variable_hardness import VariableHardness
