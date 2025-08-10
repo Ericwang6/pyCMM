31b0a67d71c5b0d05bad26b9637a2db7566ea1bf
Ericwang6 <ericwangyz@berkeley.edu>
2025-08-09 19:58:50 -0700

    add refit params

--- scripts/water_md.py
+++ scripts/water_md.py
@@ -15,7 +15,7 @@ from ase.units import fs, bar, kB
 import openmm.app as app
 
 import sys
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '../..'))
 from cmm.interfaces import CMMCalculator
 from cmm.ffxml import ForceFieldXML
 from cmm.topology import Topology
@@ -87,20 +87,21 @@ if __name__ == '__main__':
     device = 'cuda'
     torch.set_default_dtype(torch.float64)
 
-    ff_path = 'water.xml'
+    ff_path = 'water_refit.xml'
     pdb_path = 'water_216.pdb'
+    temperature = 298.15
 
     ff = ForceFieldXML(ff_path, device='cuda')
     pdb = app.PDBFile(pdb_path)
     top = Topology.fromOpenmm(pdb.topology, device)
-    system = ff.parametrize(top, use_fd_morse=True, use_polarization=True)
+    system = ff.parametrize(top, use_fd_morse=True, cutoff_sr=5.0, use_lr_dispersion=True, use_hardness_change=False)
 
     coords = torch.tensor(pdb.getPositions(asNumpy=True)._value / BOHR2NM, device=device, requires_grad=True)
     box = torch.tensor([[vec.x / BOHR2NM, vec.y / BOHR2NM, vec.z / BOHR2NM] for vec in pdb.topology.getPeriodicBoxVectors()], device=device, requires_grad=True)
 
-    # prev = read('water216_npt_298K_50ps.traj', index=-1)
+    # prev = read('npt_298K_1000ps.traj', index=-1)
     # coords = torch.tensor(prev.get_positions() / BOHR2ANG, device=device, requires_grad=True)
-    # box = torch.tensor(prev.get_cell().array / BOHR2ANG, device=device, requires_grad=False)
+    # box = torch.tensor(prev.get_cell().array / BOHR2ANG, device=device, requires_grad=True)
     # vel = prev.get_velocities()
 
     calc = CMMCalculator(system, top, coords, box)
@@ -112,12 +113,10 @@ if __name__ == '__main__':
     atoms.write('opt.xyz')
     print("Optimization finished")
 
-    # # NVT
-    temperature = 298.15
+    # NVT
     MaxwellBoltzmannDistribution(atoms, temperature_K=temperature, force_temp=True)
     Stationary(atoms)
-    # atoms.set_velocities(vel)
-    Stationary(atoms)
+
     dyn = Langevin(
         atoms,
         timestep=1.0 * fs,
@@ -125,13 +124,13 @@ if __name__ == '__main__':
         friction=0.01 / fs,
     )
 
-    traj = Trajectory('water216_nvt_298K_2ps.traj', 'a', atoms)
-    dyn.attach(traj.write, interval=5)
+    traj = Trajectory('nvt_298K.traj', 'a', atoms)
+    dyn.attach(traj.write, interval=50)
     
-    log = create_all_logger('water216_nvt_298K_2ps.log', atoms, dyn)
-    dyn.attach(log, interval=5)
+    log = create_all_logger('nvt_298K.log', atoms, dyn)
+    dyn.attach(log, interval=10)
     
-    dyn.run(1000)
+    dyn.run(2000)
 
     atoms.write('nvt.xyz')
     print("NVT finished")
@@ -144,11 +143,11 @@ if __name__ == '__main__':
                    taut=100 * fs, pressure_au=1.01325 * bar,
                    taup=1000 * fs, compressibility_au=4.57e-5 / bar)
     
-    traj = Trajectory('nvt_298K_500ps.traj', 'a', atoms)
-    dyn.attach(traj.write, interval=5)
+    traj = Trajectory('npt_298K_2000ps.traj', 'a', atoms)
+    dyn.attach(traj.write, interval=1000)
 
-    log = create_all_logger('nvt_298K_500ps.log', atoms, dyn)
-    dyn.attach(log, interval=5)
+    log = create_all_logger('npt_298K_2000ps.log', atoms, dyn)
+    dyn.attach(log, interval=50)
 
-    dyn.run(1000)
-    atoms.write('nvt_298K_500ps.xyz')
+    dyn.run(1000000)
+    atoms.write('npt_298K_2000ps.xyz')
